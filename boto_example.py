e43d0c03f73bdf07de5266bdc0abe717dad19f0d
wkd3475 <wkd3475@gmail.com>
2019-04-09 15:21:32 +0900

    make aws_s3 class

--- boto_example.py
+++ boto_example.py
@@ -1,27 +1,33 @@
 import boto
 import boto.s3.connection
 import boto.s3.key
-import auth as au
+import sys
+sys.path.insert(0, '../auth')
+import auth
 
+class aws_as():
+	conn = None
+	list_buckets = []
+	def __init__(self, access_key, secret_ket):
+		#connect to s3
+		self.conn = boto.connect_s3(
+			aws_access_key_id = access_key,
+			aws_secret_access_key = secret_key
+		)
+        
+		#get buckets
+		for bucket in self.conn.get_all_buckets():
+			self.list_buckets.append([bucket.name, bucket.creation_date])
 
-def connect_aws_as() :
-    conn = boto.connect_s3(
-        aws_access_key_id = au.access_key,
-        aws_secret_access_key = au.secret_key,
-    )
+	def show_list_buckets(self):
+		for bucket in self.list_buckets:
+			print(bucket)
 
-    return conn
+	def upload_file(self, bucket_name, file_name):
+		bucket = self.conn.get_bucket(bucket_name)
+		key = bucket.new_key(file_name)
 
-
-def list_buckets(conn) :
-    for bucket in conn.get_all_buckets():
-        print(bucket.name)
-        print(bucket.creation_date)
-
-def upload_file(conn, bucket_name, file_name) :
-    bucket = conn.get_bucket(bucket_name)
-    key = bucket.new_key(file_name)
-
-conn = connect_aws_as()
-list_buckets(conn)
-upload_file(conn, 'python-example-s3cl', 'hello.txt')
+access_key = auth.access_key
+secret_key = auth.secret_key
+a = aws_as(access_key, secret_key)
+a.show_list_buckets()
